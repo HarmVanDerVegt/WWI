46c8800d07b03f7a38451a4bc50834879e1e44cd
Harm van der Vegt <harmvegt@gmail.com>
2018-10-29 14:14:04 +0100

    Navbar

--- WWI/includes/header.php
+++ WWI/includes/header.php
@@ -1,35 +1,31 @@
 <html>
 <head>
-    <link href="../css/bootstrap.min.css" rel="stylesheet"/>
-    <link href="../css/navbar.css" rel="stylesheet"/>
-    <script src="../js/bootstrap.min.js"></script>
+    <link href="../../css/bootstrap.min.css" rel="stylesheet"/>
+    <link href="../../css/navbar.css" rel="stylesheet"/>
+    <script src="../../js/bootstrap.min.js"></script>
 </head>
 
 <nav class="navbar navbar-custom">
-    <div>
+    <div class="container-fluid">
+    <div class="navbar-header">
         <a class="navbar-brand" href="home.php">WWI</a>
+    </div>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav">
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
-                   aria-haspopup="true" aria-expanded="false">
-                    Categorieën
-                </a>
+    <ul class="nav navbar-nav">
+        <li class="dropdown">
+            <a class="dropdown-toggle" data-toggle="dropdown" href="#">Categories
+                <span class="caret"></span></a>
 
-                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="pages/toys.php">Speelgoed</a>
-                    <a class="dropdown-item" href="pages/clothing">Kleding</a>
-                    <a class="dropdown-item" href="pages/mugs.php">Mokken</a>
-                </div>
-            </li>
-
-        </ul></div>
-
-        <form class="form-inline my-2 my-lg-0">
-            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-        </form>
+            <ul class="dropdown-menu">
+                <li><a href="#">Speelgoed</a></li>
+                <li><a href="#">Kleding</a></li>
+            </ul>
+        </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
     </div>
 </nav>
 </html>

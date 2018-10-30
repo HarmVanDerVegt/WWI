cb488db9fbe306b4e7c937f8b3bfa00772b0827f
Harm van der Vegt <harmvegt@gmail.com>
2018-10-30 09:45:43 +0100

    Navbar dropdown example gefixed.

--- WWI/includes/header.php
+++ WWI/includes/header.php
@@ -1,32 +1,32 @@
 <head>
     <link href="\WWI\WWI\css\bootstrap.min.css" rel="stylesheet" type="text/css"/>
-
+<!--    <link href="\WWI\WWI\css\navbar.css" rel="stylesheet" type="text/css"/>-->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://unpkg.com/popper.js"></script>
     <script src='\WWI\WWI\js\bootstrap.bundle.min.js'></script>
 </head>
 
-<nav class="navbar navbar-custom">
-    <div class="navbar-header">
-        <a class="navbar-brand" href="/WWI/WWI/pages/template.php">WWI</a>
-    </div>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent">
+        <span class="navbar-toggler-icon"></span>
+    </button>
 
-    <ul class="navbar-nav">
-        <li class="nav-item">
-            <div class="dropdown show">
-                <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" data-flip="true" aria-haspopup="true" aria-expanded="false">
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown">
                     Dropdown
                 </a>
-
-                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuLink">
-                    <a class="dropdown-item" href="#">Action 1</a>
-                    <a class="dropdown-item" href="#">Action 1</a>
+                <div class="dropdown-menu">
+                    <a class="dropdown-item" href="#">Actie 1</a>
+                    <a class="dropdown-item" href="#">Actie 2</a>
                 </div>
-            </div>
-        </li>
-    </ul>
-    <!--    <form class="form-inline my-2 my-lg-0">
+            </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
             <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
             <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-        </form>-->
+        </form>
+    </div>
 </nav>

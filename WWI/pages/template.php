4f3f7ca3349889a54936dc08b795368c284356ec
Harm van der Vegt <harmvegt@gmail.com>
2018-10-30 10:24:00 +0100

    Example voor rows, columns en cards.

--- WWI/pages/template.php
+++ WWI/pages/template.php
@@ -4,7 +4,96 @@ if (!defined('ROOT_PATH')){
 }
 
 include(ROOT_PATH . "/includes/header.php"); ?>
-<br>
-<p>Dit is content</p>
-<br>
+
+<div class="container">
+    <div class="row">
+        <div class="col-sm">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Card Title</h5>
+                    <p class="card-text">Card Example</p>
+                    <a href="#" class="btn btn-primary">Link</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Card Title</h5>
+                    <p class="card-text">Card Example</p>
+                    <a href="#" class="btn btn-primary">Link</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Card Title</h5>
+                    <p class="card-text">Card Example</p>
+                    <a href="#" class="btn btn-primary">Link</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <br>
+    <div class="row">
+        <div class="col-sm">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Card Title</h5>
+                    <p class="card-text">Card Example</p>
+                    <a href="#" class="btn btn-primary">Link</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Card Title</h5>
+                    <p class="card-text">Card Example</p>
+                    <a href="#" class="btn btn-primary">Link</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Card Title</h5>
+                    <p class="card-text">Card Example</p>
+                    <a href="#" class="btn btn-primary">Link</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <br>
+    <div class="row">
+        <div class="col-sm">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Card Title</h5>
+                    <p class="card-text">Card Example</p>
+                    <a href="#" class="btn btn-primary">Link</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Card Title</h5>
+                    <p class="card-text">Card Example</p>
+                    <a href="#" class="btn btn-primary">Link</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Card Title</h5>
+                    <p class="card-text">Card Example</p>
+                    <a href="#" class="btn btn-primary">Link</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 <?php include(ROOT_PATH . "/includes/footer.php"); ?>

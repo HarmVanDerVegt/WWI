ff375ccf570c4f507039911565d02f4b7f0effea
Tiemen Molenaar <s1128667@student.windesheim.nl>
2018-10-30 13:56:36 +0100

    foto's toegevoegd bij categorys

--- WWI/pages/index.php
+++ WWI/pages/index.php
@@ -15,16 +15,16 @@ if (!defined('ROOT_PATH')){
         <?php include("../includes/header.php"); ?>
         <br>
         <div>
-            <a href="category/airlinenovelties.php"><h1>Airline Novelties</h1><img src="media/airline.jpg" alt="airlinenovelties"</a>
-            <a href="category/clothing.php"><h1>Clothing</h1><img src="/fotos/" alt="clothing"</a>
-            <a href="category/computingnovelties.php"><h1>Computing Novelties</h1><img src="/fotos/" alt="computingnovelties"</a>
-            <a href="category/furryfootwear.php"><h1>Furry Footwear</h1><img src="/fotos/" alt="furryfootwear"</a>
-            <a href="category/mugs.php"><h1>Mugs</h1><img src="/fotos/" alt="mugs"</a>
-            <a href="category/noveltyitems.php"><h1>Novelty Items</h1><img src="/fotos/" alt="noveltyitems"</a>
-            <a href="category/packagingmaterials.php"><h1>Packaging Materials</h1><img src="/fotos/" alt="packagingmaterials"</a>
-            <a href="category/toys.php"><h1>Toys</h1><img src="/fotos/" alt="toys"</a>
-            <a href="category/tshirts.php"><h1>T-shirts</h1><img src="/fotos/" alt="tshirts"</a>
-            <a href="category/usbnovelties.php"><h1>USB Novelties</h1><img src="/fotos/" alt="usbnovelties"</a>
+            <a href="category/airlinenovelties.php"><h1>Airline Novelties</h1><img src="media/airline.jpg" alt="airlinenovelties" height="300px" width="300px"></a>
+            <a href="category/clothing.php"><h1>Clothing</h1><img src="media/clothing.jpg" alt="clothing" height="300px" width="300px"></a>
+            <a href="category/computingnovelties.php"><h1>Computing Novelties</h1><img src="media/computingnovelties.jpg" alt="computingnovelties" height="300px" width="300px"></a>
+            <a href="category/furryfootwear.php"><h1>Furry Footwear</h1><img src="media/furryfootwear.jpg" alt="furryfootwear" height="300px" width="300px"></a>
+            <a href="category/mugs.php"><h1>Mugs</h1><img src="media/mugs.jpg" alt="mugs" height="300px" width="300px"></a>
+            <a href="category/noveltyitems.php"><h1>Novelty Items</h1><img src="media/noveltyitems.jpg" alt="noveltyitems" height="300px" width="300px"></a>
+            <a href="category/packagingmaterials.php"><h1>Packaging Materials</h1><img src="/fotos/" alt="packagingmaterials" height="300px" width="300px"></a>
+            <a href="category/toys.php"><h1>Toys</h1><img src="media/toys.jpg" alt="toys" height="300px" width="300px"></a>
+            <a href="category/tshirts.php"><h1>T-shirts</h1><img src="media/toys.jpg" alt="tshirts" height="300px" width="300px"></a>
+            <a href="category/usbnovelties.php"><h1>USB Novelties</h1><img src="media/usbnovelties.jpg" alt="usbnovelties" height="300px" width="300px"></a>
         </div>
         <br>
         <?php include("../includes/footer.php"); ?>

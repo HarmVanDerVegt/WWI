deb11266eb52f346a395e2cb99e1286e5fd20094
Justin Noppers <justin.noppers@gmail.com>
2018-10-30 13:27:36 +0100

    Voorbereiding Images

--- WWI/pages/index.php
+++ WWI/pages/index.php
@@ -15,16 +15,16 @@ if (!defined('ROOT_PATH')){
         <?php include("../includes/header.php"); ?>
         <br>
         <div>
-            <a href="category/airlinenovelties.php"><h1>Airline Novelties</h1></a>
-            <a href="category/clothing.php"><h1>Clothing</h1></a>
-            <a href="category/computingnovelties.php"><h1>Computing Novelties</h1></a>
-            <a href="category/furryfootwear.php"><h1>Furry Footwear</h1></a>
-            <a href="category/mugs.php"><h1>Mugs</h1></a>
-            <a href="category/noveltyitems.php"><h1>Novelty Items</h1></a>
-            <a href="category/packagingmaterials.php"><h1>Packaging Materials</h1></a>
-            <a href="category/toys.php"><h1>Toys</h1></a>
-            <a href="category/tshirts.php"><h1>T-shirts</h1></a>
-            <a href="category/usbnovelties.php"><h1>USB Novelties</h1></a>
+            <a href="category/airlinenovelties.php"><h1>Airline Novelties</h1><img src="/fotos/" alt="airlinenovelties"</a>
+            <a href="category/clothing.php"><h1>Clothing</h1><img src="/fotos/" alt="clothing"</a>
+            <a href="category/computingnovelties.php"><h1>Computing Novelties</h1><img src="/fotos/" alt="computingnovelties"</a>
+            <a href="category/furryfootwear.php"><h1>Furry Footwear</h1><img src="/fotos/" alt="furryfootwear"</a>
+            <a href="category/mugs.php"><h1>Mugs</h1><img src="/fotos/" alt="mugs"</a>
+            <a href="category/noveltyitems.php"><h1>Novelty Items</h1><img src="/fotos/" alt="noveltyitems"</a>
+            <a href="category/packagingmaterials.php"><h1>Packaging Materials</h1><img src="/fotos/" alt="packagingmaterials"</a>
+            <a href="category/toys.php"><h1>Toys</h1><img src="/fotos/" alt="toys"</a>
+            <a href="category/tshirts.php"><h1>T-shirts</h1><img src="/fotos/" alt="tshirts"</a>
+            <a href="category/usbnovelties.php"><h1>USB Novelties</h1><img src="/fotos/" alt="usbnovelties"</a>
         </div>
         <br>
         <?php include("../includes/footer.php"); ?>

9e393400ae0980c04e196423cd361c53b5125092
Tiemen Molenaar <s1128667@student.windesheim.nl>
2018-10-31 13:19:38 +0100

    category informatie in data/data.php gedaan en index.php verbeterd

---
+++ WWI/pages/data/data.php
@@ -0,0 +1,78 @@
+<?php
+
+// catecory informatie ophalen
+        $i = 0;
+        $category[$i] = new category_type();
+        $category[$i]->category = "Airline Novelties";
+        $category[$i]->foto_path = "media/airline.jpg";
+        $category[$i]->link = "category/airlinenovelties.php";
+
+        $i++;
+        $category[$i] = new category_type();
+        $category[$i]->category = "Clothing";
+        $category[$i]->foto_path = "media/clothing.jpg";
+        $category[$i]->link = "category/clothing.php";
+
+        $i++;
+        $category[$i] = new category_type();
+        $category[$i]->category = "Computing Novelties";
+        $category[$i]->foto_path = "media/computingnovelties.jpg";
+        $category[$i]->link = "category/computingnovelties.php";
+
+        $i++;
+        $category[$i] = new category_type();
+        $category[$i]->category = "Furry Footwear";
+        $category[$i]->foto_path = "media/furryfootwear.jpg";
+        $category[$i]->link = "category/furryfootwear.php";
+
+        $i++;
+        $category[$i] = new category_type();
+        $category[$i]->category = "Mugs";
+        $category[$i]->foto_path = "media/mugs.jpg";
+        $category[$i]->link = "category/mugs.php";
+        
+        $i++;
+        $category[$i] = new category_type();
+        $category[$i]->category = "Novelty Items";
+        $category[$i]->foto_path = "media/noveltyitems.jpg";
+        $category[$i]->link = "category/noveltyitems.php";
+        
+        $i++;
+        $category[$i] = new category_type();
+        $category[$i]->category = "Packaging Materials";
+        $category[$i]->foto_path = "media/packagingmaterials.jpg";
+        $category[$i]->link = "category/packagingmaterials.php";
+        
+        $i++;
+        $category[$i] = new category_type();
+        $category[$i]->category = "Toys";
+        $category[$i]->foto_path = "media/toys.jpg";
+        $category[$i]->link = "category/toys.php";
+        
+        $i++;
+        $category[$i] = new category_type();
+        $category[$i]->category = "T-shirts";
+        $category[$i]->foto_path = "media/tshirts.jpg";
+        $category[$i]->link = "category/tshirts.php";
+        
+        $i++;
+        $category[$i] = new category_type();
+        $category[$i]->category = "USB Novelties";
+        $category[$i]->foto_path = "media/usbnovelties.jpg";
+        $category[$i]->link = "category/usbnovelties.php";
+        
+
+?>       
+ <!--
+            !<a href="category/airlinenovelties.php"><h1>Airline Novelties</h1><img src="media/airline.jpg" alt="airlinenovelties" height="300px" width="300px"></a>
+            !<a href="category/clothing.php"><h1>Clothing</h1><img src="media/clothing.jpg" alt="clothing" height="300px" width="300px"></a>
+            !<a href="category/computingnovelties.php"><h1>Computing Novelties</h1><img src="media/computingnovelties.jpg" alt="computingnovelties" height="300px" width="300px"></a>
+            !<a href="category/furryfootwear.php"><h1>Furry Footwear</h1><img src="media/furryfootwear.jpg" alt="furryfootwear" height="300px" width="300px"></a>
+            !<a href="category/mugs.php"><h1>Mugs</h1><img src="media/mugs.jpg" alt="mugs" height="300px" width="300px"></a>
+            !<a href="category/noveltyitems.php"><h1>Novelty Items</h1><img src="media/noveltyitems.jpg" alt="noveltyitems" height="300px" width="300px"></a>
+            !<a href="category/packagingmaterials.php"><h1>Packaging Materials</h1><img src="/fotos/" alt="packagingmaterials" height="300px" width="300px"></a>
+            !<a href="category/toys.php"><h1>Toys</h1><img src="media/toys.jpg" alt="toys" height="300px" width="300px"></a>
+            !<a href="category/tshirts.php"><h1>T-shirts</h1><img src="media/toys.jpg" alt="tshirts" height="300px" width="300px"></a>
+            <a href="category/usbnovelties.php"><h1>USB Novelties</h1><img src="media/usbnovelties.jpg" alt="usbnovelties" height="300px" width="300px"></a>
+        -->
+

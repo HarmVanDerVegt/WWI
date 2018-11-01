665540a7377862a0eb42dab21ede709475395316
lex kruiper <lex.kruiper@windesheim.nl>
2018-11-01 12:46:12 +0100

    search begin

--- WWI/pages/Search.php
+++ WWI/pages/Search.php
@@ -1,7 +1,34 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: lexkruiper97
- * Date: 1-11-2018
- * Time: 10:58
- */
+if (!defined('ROOT_PATH')) {
+    include("../config.php");
+}
+include_once ROOT_PATH . "/config.php";
+include_once  ROOT_PATH . "/controllers/databaseController.php";
+
+include(ROOT_PATH . "/includes/header.php");
+
+?>
+<?php $Search = (filter_input(INPUT_GET, "Zoeken", FILTER_SANITIZE_STRING));
+
+if (filter_input(INPUT_GET, "Zoeken", FILTER_SANITIZE_STRING) <> ""){
+    var_dump(getSEARCHInfo($Search));
+}
+
+function getSEARCHInfo($search){
+    $db = createDB();
+
+    $sql = "SELECT SearchDetails
+            FROM stockitems
+            WHRERE SearchDetails like\"%$search%\" " ;
+
+    $result = $db->query($sql);
+
+
+    return $result;
+}
+
+//foreach ($Resultar as $Result){
+//    echo $Result ."/n";
+//}
+?>
+<?php  include(ROOT_PATH . "/includes/footer.php"); ?>

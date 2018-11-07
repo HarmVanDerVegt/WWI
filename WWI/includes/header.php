9947993ffddfe624f02ac6b3076e025041cf72f3
lex kruiper <lex.kruiper@windesheim.nl>
2018-11-07 13:35:35 +0100

    footer

--- WWI/includes/header.php
+++ WWI/includes/header.php
@@ -9,7 +9,6 @@
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
-
 <nav class="navbar navbar-expand-lg navbar-dark">
     <a href="/WWI/WWI/pages/index.php"><img class="img-thumbnail" src="/WWI/WWI/pages/media/wwi-ls.png" height="250px"
                                              width="90px"/>

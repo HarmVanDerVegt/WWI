416e8c7645aacf50a2bf78f0e1cc12d960e6447a
lex kruiper <lex.kruiper@windesheim.nl>
2018-11-13 13:11:39 +0100

    register update

--- WWI/pages/Register.php
+++ WWI/pages/Register.php
@@ -4,33 +4,45 @@ if (!defined('ROOT_PATH')) {
 }
 ?>
 
+
     <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
     <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
 <!-- voegt header toe -->
 <?php include(ROOT_PATH . "/includes/header.php"); ?>
 
-
-
-
-
-<html>
-<body>
-    <form>
-        <div class="form-group">
-            <label for="formGroupExampleInput">Example label</label>
-            <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Example input">
+    <div class="card mx-auto" style="width: 36rem;">
+        <div class="card-body mx-auto">
+            <div class="signup-form">
+                <form action="/examples/actions/confirmation.php" method="post">
+                    <h2>Register</h2>
+                    <p class="hint-text">Create your account. It's free and only takes a minute.</p>
+                    <div class="form-group">
+                        <div class="row">
+                            <div class="col-xs-6"><input type="text" class="form-control" name="first_name" placeholder="First Name" required="required"></div>
+                            <div class="col-xs-6"><input type="text" class="form-control" name="last_name" placeholder="Last Name" required="required"></div>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <input type="email" class="form-control" name="email" placeholder="Email" required="required">
+                    </div>
+                    <div class="form-group">
+                        <input type="password" class="form-control" name="password" placeholder="Password" required="required">
+                    </div>
+                    <div class="form-group">
+                        <input type="password" class="form-control" name="confirm_password" placeholder="Confirm Password" required="required">
+                    </div>
+                    <div class="form-group">
+                        <label class="checkbox-inline"><input type="checkbox" required="required"> I accept the <a href="#">Terms of Use</a> &amp; <a href="#">Privacy Policy</a></label>
+                    </div>
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-success btn-lg btn-block">Register Now</button>
+                    </div>
+                </form>
+                <div class="text-center">Already have an account? <a href="#">Sign in</a></div>
+            </div>
         </div>
-        <div class="form-group">
-            <label for="formGroupExampleInput2">Another label</label>
-            <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Another input">
-        </div>
-    </form>
-
-
-</body>
-</html>
-
+    </div>
 
 
 

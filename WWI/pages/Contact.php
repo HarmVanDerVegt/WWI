59f5743ba27429377c68624a0d664068695de70a
Justin Noppers <justin.noppers@gmail.com>
2018-11-19 16:05:37 +0100

    Contact

--- WWI/pages/Contact.php
+++ WWI/pages/Contact.php
@@ -17,39 +17,39 @@ if (!defined('ROOT_PATH')) {
         <?php include(ROOT_PATH . "/includes/header.php"); ?>
         <br>
         <table>
-            <form action="/WWI/pages/Contact.php" method="post">
+            <form action="" method="post">
                 <tr>
                     <td>Voornaam:</td>
-                    <td><input type="text" name="voornaam" placeholder="Voornaam"></td>
+                    <td><input type="text" name="voornaam" placeholder="Lex"></td>
                 </tr>
                 <tr>
                     <td>Achternaam:</td>
-                    <td><input type="text" name="achternaam" placeholder="Achternaam"></td>
+                    <td><input type="text" name="achternaam" placeholder="T-Rex"></td>
                 </tr>
                 <tr>
                     <td>E-mail:</td>
-                    <td><input type="email" name="email" value="Lex_De@T-Rex.nl" placeholder="E-mail" required></td>
+                    <td><input type="email" name="email" placeholder="Lex@T-Rex.nl" required></td>
                 </tr>
                 <tr>
                     <td>Bericht:</td>
-                    <td><textarea name="bericht" rows="4" placeholder="Bericht" required>bericht</textarea></td>
+                    <td><textarea name="bericht" rows="4" placeholder="Bericht" required></textarea></td>
                 </tr>
                 <tr>
-                    <td>Verzenden:</td>
-                    <td><input type="submit" value="verzenden!"></td>
+                    <td></td>
+                    <td><input type="submit" value="Verzenden!"></td>
                 </tr>
             </form>
         </table>
 
         <?php
         $i = 0;
-//if(filter_input(INPUT_POST, "email", FILTER_SANITIZE_STRING) != NULL && filter_input(INPUT_POST, "submit", FILTER_SANITIZE_STRING) == TRUE){
-        //  $i++;
-        mail("contact.wideworldimporters@gmail.com", $i, filter_input(INPUT_POST, "bericht", FILTER_SANITIZE_STRING), "FROM: " . filter_input(INPUT_POST, "email", FILTER_SANITIZE_STRING));
-//}
+        if (filter_input(INPUT_POST, "email", FILTER_SANITIZE_STRING) != NULL && filter_input(INPUT_POST, "submit", FILTER_SANITIZE_STRING) == TRUE) {
+            $i++;
+            mail("contact.wideworldimporters@gmail.com", $i, filter_input(INPUT_POST, "bericht", FILTER_SANITIZE_STRING), "FROM: " . filter_input(INPUT_POST, "email", FILTER_SANITIZE_STRING));
+        }
         ?>
         <!-- voeg footer toe -->
         <br>
-<?php include(ROOT_PATH . "/includes/footer.php"); ?>
+        <?php include(ROOT_PATH . "/includes/footer.php"); ?>
     </body>
 </html>

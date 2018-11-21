aed81b7439feee2b09d4d30d029d5c2fc74f8333
lex kruiper <lex.kruiper@windesheim.nl>
2018-11-21 12:52:04 +0100

    no message

--- WWI/pages/login.php
+++ WWI/pages/login.php
@@ -3,7 +3,7 @@
 <?php
 include_once "../controllers/userController.php";
 
-
+session_start();
 
 $username = $_POST["username"];
 $password = $_POST["password"];
@@ -12,10 +12,11 @@ $returnar = getUser($username, $password);
 
 echo "Ingelogde user is: " . $returnar["FullName"] . "<br>";
 echo "Email van user is: " . $returnar["LogonName"];
-$returnar['IsEmployee'] = $_SESSION['IsEmployee'];
-$returnar['IsSystemUser'] = $_SESSION['IsSystemUser'];
-$returnar['PreferredName'] = $_SESSION['PreferredName'];
-$returnar['FullName'] = $_SESSION['FullName'];
+$_SESSION['IsEmployee']= $returnar["IsEmployee"];
+$_SESSION['IsSystemUser']= $returnar["IsSystemUser"];
+$_SESSION['PreferredName']= $returnar["PreferredName"];
+$_SESSION['FullName']= $returnar["FullName"];
+
 
 
 ?>

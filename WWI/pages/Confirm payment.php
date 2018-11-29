a2dd832fab80dd8cb19bd3537c9d9f15a67bb491
lex kruiper <lex.kruiper@windesheim.nl>
2018-11-29 12:33:13 +0100

    mollie

---
+++ WWI/pages/Confirm payment.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lexkruiper97
+ * Date: 29-11-2018
+ * Time: 11:41
+ */
+
+
+$payment = $mollie->payments->get($payment->id);
+
+if ($payment->isPaid())
+{
+    echo "Payment received.";
+}
+
+
+$payments = $mollie->payments->page();

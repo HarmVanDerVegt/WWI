0f373cbe623d12d6d9253dcd6a6f06ce6fb15aa6
Yannick de Graaf <yannickdegraaf17@gmail.com>
2018-12-05 14:49:50 +0100

    SI senor

--- WWI/controllers/userController.php
+++ WWI/controllers/userController.php
@@ -107,7 +107,7 @@ function insertRecoveryToken($token, $ID)
 function mailRecoveryToken($token, $mail, $ID)
 {
 
-    $link = "htpp://localhost/WWI/WWI/pages/wachtwoordvergeten.php?token=" . $token . "&userID=" . $ID;
+    $link = "http://localhost:63342/WWI/WWI/pages/wachtwoordReset.php?token=" . $token . "&userID=" . $ID;
 
     $message = "Beste gebruiker,\n
                 \n
---
+++ WWI/pages/wachtwoordresetbevestiging.php
@@ -0,0 +1,23 @@
+<?php
+if (!defined('ROOT_PATH')) {
+    include("../config.php");
+}
+?>
+<?php include(ROOT_PATH . "/includes/header.php");
+include_once ROOT_PATH . "/controllers/UserController.php";
+?>
+
+
+
+<h1>JAJA het is gelukt or <3</h1>
+<?php
+$ID = filter_input(INPUT_GET, "userID", FILTER_SANITIZE_STRING);
+$password = filter_input(INPUT_POST, 'wachtwoord', FILTER_SANITIZE_STRING);
+$passwordCheck = filter_input(INPUT_POST, 'bevestig_wachtwoord', FILTER_SANITIZE_STRING);
+if (isset($password) && isset($passwordCheck)) {
+    if ($password != $passwordCheck) {
+        print("<p>wachtwoorden komen niet overeen</p>");
+    } else {
+        resetPassword($ID, $password);
+    }
+}

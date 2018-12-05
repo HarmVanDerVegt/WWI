3750a3aa308fd7e5b3af5666f9bf0c7ecc795429
Yannick de Graaf <yannickdegraaf17@gmail.com>
2018-12-05 14:10:52 +0100

    niks

--- WWI/pages/wachtwoordReset.php
+++ WWI/pages/wachtwoordReset.php
@@ -3,23 +3,36 @@ if (!defined('ROOT_PATH')) {
     include("../config.php");
 }
 ?>
-<?php include(ROOT_PATH . "/includes/header.php"); ?>
+<?php include(ROOT_PATH . "/includes/header.php");
+include_once ROOT_PATH . "/controllers/UserController.php";
+$ID = filter_input(INPUT_GET, "userID", FILTER_SANITIZE_STRING);
+$token = filter_input(INPUT_GET, "token", FILTER_SANITIZE_STRING);
 
-<div class="card mx-auto" style="width: 36rem;">
-    <div class="card-body mx-auto">
-        <div class="signup-form">
-            <div class="form-group">
-                Nieuwe wachtwoord:<input type="password" class="form-control" name="wachtwoord" placeholder="Nieuw Wachtwoord"
-                       required="required">
-            </div>
-            <div class="form-group">
-                Bevestig wachtwoord:<input type="password" class="form-control" name="bevestig_wachtwoord"
-                       placeholder="Bevestig Wachtwoord" required="required"><br>
-                <input style="width: 200px;" type="submit" class="btn btn-sample" value="Reset wachtwoord">
+$tokenValidity = checkRecoveryToken($token, $ID);
+if($tokenValidity){
+?>
+    <div class="card mx-auto" style="width: 36rem;">
+        <div class="card-body mx-auto">
+            <div class="signup-form">
+                <div class="form-group">
+                    Nieuwe wachtwoord:<input type="password" class="form-control" name="wachtwoord"
+                                             placeholder="Nieuw Wachtwoord"
+                                             required="required">
+                </div>
+                <div class="form-group">
+                    Bevestig wachtwoord:<input type="password" class="form-control" name="bevestig_wachtwoord"
+                                               placeholder="Bevestig Wachtwoord" required="required"><br>
+                    <input style="width: 200px;" type="submit" class="btn btn-sample" value="Reset wachtwoord">
+                </div>
             </div>
         </div>
     </div>
-</div>
+<?php }else{ ?>
+    <meta http-equiv="refresh" content="=0;URL=error.php"/>
+ <?php   }
+    $password = filter_input(INPUT_GET, wachtwoord, FILTER_SANITIZE_STRING);
+    $passwordCheck = filter_input(INPUT_GET, bevestig_wachtwoord, FILTER_SANITIZE_STRING);
+if(isset(resetPassword( ,)))
 
 
 <?php include_once ROOT_PATH . "/includes/footer.php"; ?>
